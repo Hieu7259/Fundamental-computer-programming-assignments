af6167850b03978961ffe4669b7f9c28a6f4bf96
Hieu <prohieu70000@gmail.com>
2026-01-23 22:58:14 +0700

    Delete Ex 4.py

--- Ex 4.py
+++
@@ -1,5 +0,0 @@
-chieudai = input("Enter length: ")
-chieurong = input("Enter width: ")
-chuvi = int(chieudai) * 2 + int(chieurong) * 2
-dientich = int(chieudai) * int(chieurong)
-print("The perimeter of the rectangle is", chuvi, "and the area of the rectangle is", dientich)
