339e3ef9a968680a330530d7edbac0f227b4ed92
Hieu <prohieu70000@gmail.com>
2026-02-04 22:00:13 +0700

    Update Ex 6.py

--- Assignment-3/Ex 6.py
+++ Assignment-3/Ex 6.py
@@ -1,8 +1,9 @@
-def acronym(phase):
-    words = phase.split()
+def acronym(phrase):
+    words = phrase.split()
     acronym_letters = ""
     for word in words:
         acronym_letters += word[0].upper()
     return acronym_letters
 bruh = input("Enter your phrase: ")
+
 print(acronym(bruh))
